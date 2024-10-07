afd36f049bcc19716e5e28a4784d667ecae032be
Bolaji06 <joshspencer0603@gmail.com>
2024-10-07 22:37:32 +0100

    changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "removeLogs" is now active!');
 
   const disposable = vscode.commands.registerCommand(
     "extension.removeLogs",
